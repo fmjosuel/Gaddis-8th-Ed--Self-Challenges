90fbc49ac741a6337ff51a97cad248b73675a700
Josuel <joshlixmus@gmail.com>
2015-04-14 22:24:28 -0400

    Update Arrays2_stuck.cpp

--- Arrays2_stuck.cpp
+++ Arrays2_stuck.cpp
@@ -1,3 +1,23 @@
+/*
+12. Grade Book
+A teacher has five students who have taken four tests. The teacher uses the following
+grading scale to assign a letter grade to a student, based on the average of his or her
+four test scores.
+Test Score Letter Grade
+90–100 A
+80–89 B
+70–79 C
+60–69 D
+0–59 F
+Write a program that uses an array of string objects to hold the five student names,
+an array of five characters to hold the five students’ letter grades, and five arrays of
+four double s to hold each student’s set of test scores.
+The program should allow the user to enter each student’s name and his or her four
+test scores. It should then calculate and display each student’s average test score and a
+letter grade based on the average.
+Input Validation: Do not accept test scores less than 0 or greater than 100.
+*/
+
 #include <iostream>
 
 using namespace std;
